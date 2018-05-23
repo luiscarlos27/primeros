91cd5c2f1ee7547da96fcb2534caf92ae9a64210
Carlos Vega <c32veg27@gmail.com>
2018-05-22 21:15:48 -0600

    Primeng is added to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '- Nueva App con Angular 5 y primeng 6';
 }
