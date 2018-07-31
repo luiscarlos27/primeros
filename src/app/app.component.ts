71891f4a8c9d2314dbef17a1e561e59a424e88cf
Carlos Vega <c32veg27@gmail.com>
2018-07-31 08:48:21 -0600

    Se elimina el componente dual-listbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit  } from '@angular/core';
-import { DualListComponent } from 'angular-dual-listbox';
+//import { DualListComponent } from 'angular-dual-listbox';
 
 declare var $: any;
 @Component({
