d5b0cd27e84791bf1c9a1ed34cb36f4c4876ee26
Carlos Vega <c32veg27@gmail.com>
2018-08-04 10:37:02 -0600

    Cambios en el archivo de configuracion app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { Component } from '@angular/core'
-
-declare var $: any;
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-    title = '- Nueva App con Angular 5 y primeng 6'
-    hola = 'dsfdf'
+export class AppComponent {
+  title = '- Nueva App con Angular 5 y primeng 6';
 }
